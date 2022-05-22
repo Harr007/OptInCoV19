50228a1d44cb7e0f3c545080d9685e7622e79ede
Harr007 <85876032+Harr007@users.noreply.github.com>
2022-05-22 15:52:25 +0100

    Update epidem.m

--- epidem.m
+++ epidem.m
@@ -6,10 +6,8 @@ function [y,dy] = epidem(dt, x, beta, u, v, gamma_i, gamma_d, gamma_a, ksi_i, ks
     dy(2,1) = beta*(1 - u)*x(1,1)*x(2,1) - gamma_i*x(2,1) - ksi_i*x(2,1) - v*x(2,1); %Infected undetected State
     dy(3,1) = v*x(2,1) - gamma_d*x(3,1) - ksi_d*x(3,1); %Detected infected State
     dy(4,1) = ksi_i*x(2,1) +  ksi_d*x(3,1) - gamma_a*x(4,1) - mu*x(4,1); %Acutely symptomatic State
-    dy(5,1) = gamma_i*x(2,1) +  gamma_d*x(3,1) + gamma_a*x(4,1) + psi_hat*x(5,1); %Recovered State
+    dy(5,1) = gamma_i*x(2,1) +  gamma_d*x(3,1) + gamma_a*x(4,1) - psi_hat*x(5,1); %Recovered State
     dy(6,1) = mu*x(4,1); %Extinct (Deceased) State
     dy(7,1) = zeta*x(1,1) - psi*x(7,1); %Vaccinated State
     y = x + dt*dy; %State update
-
-
 
