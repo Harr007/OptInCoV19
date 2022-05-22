1a5bdbcba7b727625cf3f0bfc8cb5f84e496c444
Harr007 <85876032+Harr007@users.noreply.github.com>
2022-05-22 15:53:24 +0100

    Update pontr.m

--- pontr.m
+++ pontr.m
@@ -3,6 +3,7 @@
 function [y,dy] = pontr(dt, l, x, u, v, zeta, beta, gamma_i, gamma_d, gamma_a, ksi_i, ksi_d,mu, Q, psi, psi_hat)
     
     %Equations based on Pontryagin's minimum principle
+
     dy = -[beta*x(2,1)*(l(2,1) - l(1,1))*(1-u) + zeta*(l(7,1) - l(1,1));
     beta*x(1,1)*(l(2,1) - l(1,1))*(1-u) + v*(l(3,1)-l(2,1)) + gamma_i*(l(5,1) - l(2,1)) + ksi_i*(l(4,1) - l(2,1));
     gamma_d*(l(5,1) - l(3,1))  + ksi_d*(l(4,1) - l(3,1));
@@ -11,18 +12,3 @@ function [y,dy] = pontr(dt, l, x, u, v, zeta, beta, gamma_i, gamma_d, gamma_a, k
     0;
     psi*(l(1,1) - l(7,1))];
     y = l - dy*dt; %backwards in time, since boundary condition for costate variables is at t = T
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
